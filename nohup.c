375355e8eeb439996b5fc5b8731aa4ab88e3b64b
Aleksandr Pavozkov <99251771+apavozkov@users.noreply.github.com>
2024-11-02 17:20:06 +0300

    Update nohup.c

--- nohup.c
+++ nohup.c
@@ -7,50 +7,47 @@
 #include <signal.h>
 
 void detach() {
-    pid_t pid = fork();
+    pid_t pid = fork(); // Создаём новый процесс
 
-    if (pid < 0) {
+    if (pid < 0) { // Проверяем, не возникло ли ошибки при создании процесса, остальные значения нас устраивают
         printf("Не удалось создать новый процесс");
         exit;
     }
 
-    if (pid == 0) {
-        if (setsid() < 0) {
+    if (pid == 0) { // Мы в дочернем процессе
+        if (setsid() < 0) { // Создаём новый сеанс, проверяем успешность этого действия
             printf("Не удалось создать новый сеанс");
             exit;
         }
 
-        close(STDIN_FILENO);
+        close(STDIN_FILENO); // Обрубаем зависимость от тьерминала, закрывая дескрипторы ввода, вывода и ошибок
         close(STDOUT_FILENO);
         close(STDERR_FILENO);
 
-        int fd = open("/dev/null", O_RDWR);
-        if (fd < 0) {
-            print("Не удалось открыть /dev/null");
+        int fd = open("/dev/null", O_RDWR); // Открываем /dev/null и проверяем успешность
+        if (fd < 0) { 
+            printf("Не удалось открыть /dev/null");
             exit;
         }
 
-        dup2(fd, STDIN_FILENO);
+        dup2(fd, STDIN_FILENO); // НАправляем дескрипторы в /dev/null
         dup2(fd, STDOUT_FILENO);
         dup2(fd, STDERR_FILENO);
 
-        char *args[] = {"path_program", NULL}; 
+        char *args[] = {"path_program", NULL}; // Запускаем целевую программу
         execvp(args[0], args);
-        
-        perror("execvp failed");
+        printf("Не удалось запустить целевую программу");
         exit;
     }
 }
 
 int main(int argc, char *argv[]) {
-    // Проверка количества аргументов
-    if (argc < 2) {
+    if (argc < 2) { // Проверяем входные данные
         printf("Ошибка во входных данных");
         exit;
     }
 
-    detach();
+    detach(); // Запускаем основную рабочую функцию
 
-    // Завершаем родительский процесс
-    return EXIT_SUCCESS;
+    return 0;
 }
