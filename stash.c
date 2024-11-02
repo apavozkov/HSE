238c3bdde6249b13dc77209e43adacd85704ec70
Aleksandr Pavozkov <99251771+apavozkov@users.noreply.github.com>
2024-11-02 19:55:37 +0300

    Update stash.c

--- stash.c
+++ stash.c
@@ -6,6 +6,7 @@
 #define MAGIC_NUMBER_SIZE 8  // Размер магического числа
 #define OFFSET 0              // Смещение магического числа в файле
 #define START_XOR_KEY 0xFF         // Стартовый ключ для XOR
+#define STEP_KEY 0x0A        // Размер шага до ключа следующего байта
 
 void distort(const char *filename) {
     FILE *file = fopen(filename, "rb+"); // Открываем файлв режиме чтение+запись и проверяем успешность
@@ -17,24 +18,24 @@ void distort(const char *filename) {
     unsigned char magic[MAGIC_NUMBER_SIZE];
     
     fseek(file, OFFSET, SEEK_SET); // Читаем текущее magicnum с 0 позиции на указанное число байт
-    fread(magic, 1, MAGIC_NUMBER_SIZE, file); // Записываем в массив
+    fread(magic, 1, MAGIC_NUMBER_SIZE, file); 
     
-    int dkey = START_XOR_KEY;
+    int dkey = START_XOR_KEY; // Ставим в переменную ключа стартовое значение
 
-    for (int i = 0; i < MAGIC_NUMBER_SIZE; i++) {
-        int dkey = 0x0A + dkey;
-        magic[i] ^= dkey;
+    for (int i = 0; i < MAGIC_NUMBER_SIZE; i++) { // Перебираем в цикле все байты
+        int dkey = STEP_KEY + dkey; // Создаётся новый ключ для следующего байта
+        magic[i] ^= dkey; // Искажаем значение через XOR
     }
     
-    fseek(file, OFFSET, SEEK_SET);
+    fseek(file, OFFSET, SEEK_SET); // Возвращаем искаженное magicnum обратно в файл
     fwrite(magic, 1, MAGIC_NUMBER_SIZE, file);
     
-    fclose(file);
+    fclose(file);  // Закрываем файл и выводим сообщение с результатом
     printf("Файл искажен");
 }
 
 void restore(const char *filename) {
-    FILE *file = fopen(filename, "rb+");
+    FILE *file = fopen(filename, "rb+"); // Открываем файлв режиме чтение+запись и проверяем успешность
     if (!file) {
         printf("Не удалось открыть файл");
         exit;
@@ -42,30 +43,30 @@ void restore(const char *filename) {
     
     unsigned char magic[MAGIC_NUMBER_SIZE];
   
-    fseek(file, OFFSET, SEEK_SET);
-    fread(magic, 1, MAGIC_NUMBER_SIZE, file);
+    fseek(file, OFFSET, SEEK_SET); // Читаем текущее magicnum с 0 позиции на указанное число байт
+    fread(magic, 1, MAGIC_NUMBER_SIZE, file); // Записываем в массив
     
-    int rkey = START_XOR_KEY;
+    int rkey = START_XOR_KEY; // Ставим в переменную ключа стартовое значение
 
-    for (int i = 0; i < MAGIC_NUMBER_SIZE; i++) {
-        int rkey = 0x0A + rkey;
-        magic[i] ^= rkey;
+    for (int i = 0; i < MAGIC_NUMBER_SIZE; i++) { // Перебираем в цикле все байты
+        int rkey = STEP_KEY + rkey; // Создаётся новый ключ для следующего байта
+        magic[i] ^= rkey; // Искажаем значение через XOR
     }
     
-    fseek(file, OFFSET, SEEK_SET);
+    fseek(file, OFFSET, SEEK_SET);  // Возвращаем искаженное magicnum обратно в файл
     fwrite(magic, 1, MAGIC_NUMBER_SIZE, file);
     
-    fclose(file);
+    fclose(file); // Закрываем файл и выводим сообщение с результатом
     printf("Файл восстановлен");
 }
 
 int main(int argc, char **argv) {
-    if (argc != 3) {
+    if (argc != 3) { // Проверяем входные данные
         printf("Ошибка во входных данных");
         exit;
     }
 
-    if (strcmp(argv[1], "distort") == 0) {
+    if (strcmp(argv[1], "distort") == 0) { // Вызываем функцию в зависимости от режима запуска
         distort(argv[2]);
     } else if (strcmp(argv[1], "restore") == 0) {
         restore(argv[2]);
