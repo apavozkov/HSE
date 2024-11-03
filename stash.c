6204560a29fc97a2e0567c04f964650a250620de
Aleksandr Pavozkov <99251771+apavozkov@users.noreply.github.com>
2024-11-03 19:30:40 +0300

    Update stash.c

--- stash.c
+++ stash.c
@@ -8,7 +8,7 @@
 #define START_XOR_KEY 0xFF         // Стартовый ключ для XOR
 #define STEP_KEY 0x0A        // Размер шага до ключа следующего байта
 
-void distort(const char *filename) {
+void alteration (const char *filename) {
     FILE *file = fopen(filename, "rb+"); // Открываем файлв режиме чтение+запись и проверяем успешность
     if (!file) {
         printf("Не удалось открыть файл");
@@ -23,7 +23,7 @@ void distort(const char *filename) {
     int dkey = START_XOR_KEY; // Ставим в переменную ключа стартовое значение
 
     for (int i = 0; i < MAGIC_NUMBER_SIZE; i++) { // Перебираем в цикле все байты
-        int dkey = STEP_KEY + dkey; // Создаётся новый ключ для следующего байта
+        int dkey = STEP_KEY + dkey; // Создаётся новый ключ для байта
         magic[i] ^= dkey; // Искажаем значение через XOR
     }
     
@@ -34,32 +34,6 @@ void distort(const char *filename) {
     printf("Файл искажен");
 }
 
-void restore(const char *filename) {
-    FILE *file = fopen(filename, "rb+"); // Открываем файлв режиме чтение+запись и проверяем успешность
-    if (!file) {
-        printf("Не удалось открыть файл");
-        exit;
-    }
-    
-    unsigned char magic[MAGIC_NUMBER_SIZE];
-  
-    fseek(file, OFFSET, SEEK_SET); // Читаем текущее magicnum с 0 позиции на указанное число байт
-    fread(magic, 1, MAGIC_NUMBER_SIZE, file); // Записываем в массив
-    
-    int rkey = START_XOR_KEY; // Ставим в переменную ключа стартовое значение
-
-    for (int i = 0; i < MAGIC_NUMBER_SIZE; i++) { // Перебираем в цикле все байты
-        int rkey = STEP_KEY + rkey; // Создаётся новый ключ для следующего байта
-        magic[i] ^= rkey; // Искажаем значение через XOR
-    }
-    
-    fseek(file, OFFSET, SEEK_SET);  // Возвращаем искаженное magicnum обратно в файл
-    fwrite(magic, 1, MAGIC_NUMBER_SIZE, file);
-    
-    fclose(file); // Закрываем файл и выводим сообщение с результатом
-    printf("Файл восстановлен");
-}
-
 int main(int argc, char **argv) {
     if (argc != 3) { // Проверяем входные данные
         printf("Ошибка во входных данных");
@@ -67,9 +41,11 @@ int main(int argc, char **argv) {
     }
 
     if (strcmp(argv[1], "distort") == 0) { // Вызываем функцию в зависимости от режима запуска
-        distort(argv[2]);
+        alteration(argv[2]);
+        printf("Файл искажен");
     } else if (strcmp(argv[1], "restore") == 0) {
-        restore(argv[2]);
+        alteration(argv[2]);
+        printf("Файл восстановлен");
     } else {
         printf("Неверная команда: %s. Используйте 'distort' или 'restore'.\n");
         exit;
